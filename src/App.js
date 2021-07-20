be549d8fed4b424d32380dce65e791f8665771a6
shimon tadesa <shimontadesa123@gmail.com>
2021-07-20 18:39:42 +0300

    add new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const api = {
+    key: "8a489eff0aeb124a757471cc717287de",
+    base: "https://home.openweathermap.org/data/2.5/",
+  };
+
+  return <div className="App">hello</div>;
 }
 
 export default App;
